20a9b254bc5bea03376216707a5ea412cb1e6fe2
TastyBao <erard.bastien@gmail.com>
2022-10-26 11:11:46 +0200

    Update

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: berard <berard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 17:56:29 by berard            #+#    #+#             */
-/*   Updated: 2022/10/25 18:13:13 by berard           ###   ########.fr       */
+/*   Updated: 2022/10/26 10:54:13 by berard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@
 # include <stdlib.h>
 # include <ctype.h>
 
-int	ft_atoi(const char *str)
-int	ft_isalnum(int c)
-int	ft_isalpha(int c)
-int	ft_isascii(int c)
-int	ft_isdigit(int c)
-int	ft_isprint(int c)
-int	ft_strlen(char *str)
-int	ft_tolower(int c)
-int	ft_toupper(int c)
-void	ft_putchar_fd(char c, int fd)
-void	ft_putendl_fd(char *s, int fd)
-void	ft_putnbr_fd(int n, int fd)
-void	ft_putstr_fd(char *s, int fd)
+int	ft_atoi(const char *str);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+int	ft_strlen(char *str);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
