d4ca22c1f7e5bccc44c30d08bfb8791d89d70603
TastyBao <erard.bastien@gmail.com>
2022-10-31 17:41:44 +0100

    Update

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: berard <berard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 16:04:53 by berard            #+#    #+#             */
-/*   Updated: 2022/10/31 16:08:55 by berard           ###   ########.fr       */
+/*   Updated: 2022/10/31 17:41:30 by berard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,29 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	char	*ptr;
+	int		i;
 
+	i = 0;
+	if (ft_strlen(s) <= start)
+	{
+		len = 0;
+	}
+	if (ft_strlen(s) - start < len)
+	{
+		len = ft_strlen(s) - start;
+	}
+	ptr = malloc(sizeof(char const) * (len + 1));
+	if (ptr == 0)
+	{
+		return (NULL);
+	}
+	while (len > 0)
+	{
+		ptr[i] = s[start + i];
+		i++;
+		len --;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
