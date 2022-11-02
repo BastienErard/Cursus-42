9239fdc41a9701f8aed5c133ce713eff2b7bfc9f
TastyBao <erard.bastien@gmail.com>
2022-11-02 09:40:53 +0100

    Update

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: berard <berard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 11:31:39 by berard            #+#    #+#             */
-/*   Updated: 2022/11/01 18:07:59 by berard           ###   ########.fr       */
+/*   Updated: 2022/11/02 09:35:48 by berard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,26 +22,18 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	{
 		return ((char *)haystack);
 	}
-	while (haystack[i] != '\0' && i <= len)
+	while (haystack[i] != '\0' && i < len)
 	{
 		j = 0;
-		while (haystack[i] == needle[j] || needle[j] == '\0')
+		while (haystack[i + j] == needle[j] && i + j < len)
 		{
-			if (needle[j] == '\0' && i <= len)
+			if (needle[j + 1] == '\0')
 			{
-				return (&((char *)(haystack))[i - j]);
+				return ((char *)&haystack[i]);
 			}
 			j++;
-			i++;
 		}
-		i = i - j;
 		i++;
 	}
 	return (NULL);
 }
-
-/*int	main(void)
-{
-	printf("%s", ft_strnstr("abcdefgh", "abc", 2));
-	return (0);
-}*/
