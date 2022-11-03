4681fa5b1a6a70901ed6bd7a29e0ea4a13366c21
TastyBao <erard.bastien@gmail.com>
2022-11-03 14:44:32 +0100

    Update

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,24 +6,27 @@
 /*   By: berard <berard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 11:05:08 by berard            #+#    #+#             */
-/*   Updated: 2022/11/02 13:00:22 by berard           ###   ########.fr       */
+/*   Updated: 2022/11/03 14:44:20 by berard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_strcpy(char *dst, const char *src)
+static int	ft_sizemalloc(int n)
 {
 	int	i;
 
 	i = 0;
-	while (dst[i] != '\0')
+	while (n > 9 || n < -9)
 	{
-		dst[i] = src[i];
+		n = n / 10;
 		i++;
 	}
-	dst[i] = '\0';
-	return (dst);
+	if (n < 0)
+	{
+		i++;
+	}
+	return (i + 1);
 }
 
 char	*ft_itoa(int n)
@@ -31,26 +34,25 @@ char	*ft_itoa(int n)
 	char	*ptr;
 	int		i;
 
-	i = 0;
-	ptr = malloc(sizeof(char) * 12);
+	if (n == -2147483648)
+		return (ptr = ft_strdup("-2147483648"));
+	i = ft_sizemalloc(n);
+	ptr = malloc(sizeof(char) * (i + 1));
 	if (ptr == 0)
 		return (NULL);
-	if (n == -2147483648)
-		return (ft_strcpy(ptr, "-2147483648"));
-	else if (n < 0)
+	ptr[i] = '\0';
+	if (n < 0)
 	{
 		ptr[0] = '-';
-		ft_itoa(n * -1);
+		n = n * -1;
 	}
-	else if (n >= 0 && n <= 9)
+	if (n == 0)
+		ptr[0] = 48;
+	while (n != '\0')
 	{
-		if (ptr[0] == '-')
-			i++;
-		ptr[i] = n + 48;
+		ptr[i - 1] = n % 10 + 48;
+		n = n / 10;
+		i--;
 	}
-	else
-		return(ft_strcpy(ft_itoa(n / 10), ft_itoa(n % 10)));
-	i++;
-	ptr[i] = '\0';
 	return (ptr);
 }
