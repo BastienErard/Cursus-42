adf8ee1f23cd451e8926097b6c676cacf8f212d7
TastyBao <erard.bastien@gmail.com>
2022-11-09 18:16:31 +0100

    Update

---
+++ Printf/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: berard <berard@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 16:48:17 by berard            #+#    #+#             */
+/*   Updated: 2022/11/09 18:09:23 by berard           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <ctype.h>
+# include <string.h>
+# include <stdarg.h>
+
+FONCTION
+FONCTION
+FONCTION
+FONCTIOOOOOOOON
+
+#endif
