8dd669a002ef432f6e7542877003762cc08b7b45
TastyBao <erard.bastien@gmail.com>
2022-11-10 18:06:55 +0100

    Update

--- Printf/ft_printf.c
+++ Printf/ft_printf.c
@@ -6,11 +6,37 @@
 /*   By: berard <berard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:46:19 by berard            #+#    #+#             */
-/*   Updated: 2022/11/09 18:14:42 by berard           ###   ########.fr       */
+/*   Updated: 2022/11/10 18:06:43 by berard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	ft_params(va_list args, char input)
+{
+	int	len;
+
+	len = 0;
+	if (input == 'c')
+		len += ft_print_char(va_arg(args, int));
+	else if (input == 's')
+		len += ft_print_str(va_arg(args, char *));
+	else if (input == 'p')
+		len += ft_print_ptr(va_arg(args, unsigned long));
+	else if (input == 'd')
+		len += ft_print_dec(va_arg(args, int));
+	else if (input == 'i')
+		len += ft_print_ent(va_arg(args, int));
+	else if (input == 'u')
+		len += ft_print_uent(va_arg(args, unsigned int));
+	else if (input == 'x')
+		len += ft_print_nbhexmin(va_arg(args, unsigned int));
+	else if (input == 'X')
+		len += ft_print_nbhexmaj(va_arg(args, unsigned int));
+	else if (input == '%')
+		len += ft_print_prc();
+	return (len);
+}
 
 int	ft_printf(const char *input, ...)
 {
@@ -20,11 +46,17 @@ int	ft_printf(const char *input, ...)
 
 	i = 0;
 	len = 0;
+	va_start (args, input);
 	while (input[i] != '\0')
 	{
 		if (input[i] == '%')
 		{
-
+			len += ft_params(args, input[i + 1]);
+			i++;
+		}
+		else
+		{
+			len += ft_others(input[i]);
 		}
 		i++;
 	}
--- ft_print_char.c
+++ ft_print_char.c
@@ -1,26 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_print_char.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: berard <berard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/09 16:48:17 by berard            #+#    #+#             */
-/*   Updated: 2022/11/10 18:05:33 by berard           ###   ########.fr       */
+/*   Created: 2022/11/10 18:05:02 by berard            #+#    #+#             */
+/*   Updated: 2022/11/10 18:05:55 by berard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <ctype.h>
-# include <string.h>
-# include <stdarg.h>
-
-int	ft_printf(const char *input, ...);
-int	ft_print_char(int c);
-
-#endif
+int	ft_print_char(int c)
+{
+	write(1, &c, 1);
+	return (1);
+}
