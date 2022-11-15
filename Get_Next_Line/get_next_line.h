0f884973002f06d8ef498862126f4ba0595f6437
TastyBao <erard.bastien@gmail.com>
2022-11-15 17:58:48 +0100

    Update

---
+++ Get_Next_Line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: berard <berard@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 16:54:51 by berard            #+#    #+#             */
+/*   Updated: 2022/11/15 17:00:16 by berard           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
