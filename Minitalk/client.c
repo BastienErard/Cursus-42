9a704abccecc27545ba416f07a1cb349a7fb6ba1
TastyBao <erard.bastien@gmail.com>
2022-12-19 14:10:35 +0100

    Update

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: berard <berard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 11:14:41 by berard            #+#    #+#             */
-/*   Updated: 2022/12/19 11:24:28 by berard           ###   ########.fr       */
+/*   Updated: 2022/12/19 14:10:35 by berard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 int	main(int argc, char *argv[])
 {
+	int	pid;
+
+	pid = ft_atoi(argv[1]);
 	if (argc == 3)
 	{
 
 	}
 	else
 	{
-
+		printf("Please use the correct PID with only one other argument.");
 	}
 	return (0);
 }
