5e6af9936a216debadcb9eade9175abd729ac6c6
TastyBao <erard.bastien@gmail.com>
2022-12-30 16:29:00 +0100

    Update

--- Push_Swap/push_swap.c
+++ Push_Swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: berard <berard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 14:33:12 by berard            #+#    #+#             */
-/*   Updated: 2022/12/30 14:23:33 by berard           ###   ########.fr       */
+/*   Updated: 2022/12/30 16:27:47 by berard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	main(int argc, char *argv[])
 {
+	int	*a;
+
 	if (argc == 1)
 		return (0);
 	if (argc == 2)
@@ -23,7 +25,16 @@ int	main(int argc, char *argv[])
 		ft_putstr_fd("Error\n", 2);
 		return (0);
 	}
-	printf("Super");
-	// free(argv);
+	a = ft_create_stack(argc, argv);
+	printf("%d", a[2]);
+	free(argv);
 	return (0);
 }
+
+
+//Etape à suivre :
+// 1) Création des stacks (malloc, comptage des éléments, etc...)
+// 2) Verification des doublons
+// 3) Création des différents mouvements dans le projet
+// Quicksort? Radix sort? Median avec pivot? Insertion sort?
+// A priori, radix sort.
--- Push_Swap/stacks.c
+++ Push_Swap/stacks.c
@@ -1,29 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   stacks.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: berard <berard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/26 16:42:14 by berard            #+#    #+#             */
-/*   Updated: 2022/12/30 16:22:08 by berard           ###   ########.fr       */
+/*   Created: 2022/12/30 15:55:50 by berard            #+#    #+#             */
+/*   Updated: 2022/12/30 16:26:10 by berard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-# include "Printf/ft_printf.h"
+int	*ft_create_stack(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+	int	size;
+	int	*stack;
 
-// Check_error
-int		ft_check_error(int argc, char *argv[]);
-int		ft_check_sign(char *str);
-int		ft_check_int(char *str);
-
-// Alternative functions
-long	ft_atol(const char *str);
-
-// Stacks
-int		*ft_create_stack(int argc, char *argv[]);
-
-#endif
+	i = 1;
+	size = 0;
+	if (argc == 2)
+		i = 0;
+	j = i;
+	while (argv[i])
+	{
+		i++;
+		size++;
+	}
+	i = 0;
+	stack = (int *)malloc(sizeof(int) * size);
+	if (!stack)
+		return (0);
+	while (argv[j])
+	{
+		stack[i] = ft_atoi(argv[j]);
+		i++;
+		j++;
+	}
+	return (stack);
+}
