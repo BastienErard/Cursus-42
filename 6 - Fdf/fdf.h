57598a9f6356b593adf6eb171182956bfe82f0a9
TastyBao <erard.bastien@gmail.com>
2023-02-10 19:09:12 +0100

    Update

--- 6 - Fdf/fdf.h
+++ 6 - Fdf/fdf.h
@@ -1,32 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf.c                                              :+:      :+:    :+:   */
+/*   fdf.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tastybao <tastybao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/02 16:37:00 by berard            #+#    #+#             */
-/*   Updated: 2023/02/10 18:11:01 by tastybao         ###   ########.fr       */
+/*   Created: 2023/02/10 16:39:19 by tastybao          #+#    #+#             */
+/*   Updated: 2023/02/10 17:38:01 by tastybao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "fdf.h"
+#ifndef FDF_H
+# define FDF_H
 
-int	main(int argc, char *argv[])
-{
+# include "libft/libft.h"
+# include "minilibx/mlx.h"
+# include <math.h>
 
-	int	fd;
-
-	if (argc != 2)
-	{
-		ft_putstr_fd("Please enter the map and press Enter.\n", 2);
-		return (0);
-	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 1)
-	{
-		ft_putstr_fd("The file is corrupt\n", 2);
-		return (0);
-	}
-return (0);
-}
+#endif
