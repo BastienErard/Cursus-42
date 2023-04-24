7ff1c32b72c032a8e85ffcf66aaa026875728616
TastyBao <erard.bastien@gmail.com>
2023-04-24 17:40:16 +0200

    Minor changes

--- Exam_Rank_3/ft_printf.c
+++ Exam_Rank_3/ft_printf.c
@@ -6,15 +6,17 @@
 /*   By: berard <berard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 15:41:54 by berard            #+#    #+#             */
-/*   Updated: 2023/04/24 16:08:19 by berard           ###   ########.fr       */
+/*   Updated: 2023/04/24 17:37:49 by berard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdarg.h>
 
-void	ft_putstr(char *str, int *len)
+void	ft_putstr_printf(char *str, int *len)
 {
 	int	i;
 
@@ -22,10 +24,10 @@ void	ft_putstr(char *str, int *len)
 	if (!str)
 		str = "(null)";
 	while (str[++i] != '\0')
-		len += write(1, &str[i], 1);
+		*len += write(1, &str[i], 1);
 }
 
-void	ft_putnbr(long long int nb, int base, int *len)
+void	ft_putnbr_printf(long long int nb, int base, int *len)
 {
 	char	*hexa;
 
@@ -33,14 +35,14 @@ void	ft_putnbr(long long int nb, int base, int *len)
 	if (nb < 0)
 	{
 		nb *= -1;
-		len += write(1, "-", 1);
+		*len += write(1, "-", 1);
 	}
 	if (nb >= base)
-		ft_putnbr((nb / base), base, len);
-	len += write(1, &hexa[nb % base], 1);
+		ft_putnbr_printf((nb / base), base, len);
+	*len += write(1, &hexa[nb % base], 1);
 }
 
-int	ft_printf(const char *format, ... )
+int	ft_printf(const char *format, ...)
 {
 	va_list	arg;
 	int		len;
@@ -51,35 +53,19 @@ int	ft_printf(const char *format, ... )
 	va_start(arg, format);
 	while (format[++i] != '\0')
 	{
-		if (format[i] == '%' && format[i + 1] == 's')
+		if (format[i] == '%' && (format[i + 1] == 's' || format[i + 1] == 'd' || format[i + 1] == 'x'))
 		{
-			ft_putstr(va_arg(arg, char *), &len);
-			i++;
-		}
-		else if (format[i] == '%' && format[i + 1] == 'd')
-		{
-			ft_putnbr(va_arg(arg, long long int), 10, &len);
-			i++;
-		}
-		else if (format[i] == '%' && format[i + 1] == 'x')
-		{
-			ft_putnbr(va_arg(arg, long long int), 16, &len);
-			i++;
+			i++;a
+			if (format[i] == 's')
+				ft_putstr_printf(va_arg(arg, char *), &len);
+			else if (format[i] == 'd')
+				ft_putnbr_printf((long long int)va_arg(arg, int), 10, &len);
+			else if (format[i] == 'x')
+				ft_putnbr_printf((long long int)va_arg(arg, unsigned int), 16, &len);
 		}
 		else
 			len += write(1, &format[i], 1);
 	}
 	va_end(arg);
-	printf("Len is %d\n", len);
 	return (len);
-}
-
-int	main(void)
-{
-	char	str[] = "this is sparta";
-	int		nb	= 42;
-
-	ft_printf("a\n");
-	// ft_printf("The string is %s.\n The nb is %d.\n The hexa is %x.\n", str, nb, nb);
-
 }
