649f7567512d7cb614a2a751aef4f82e60373e6a
TastyBao <erard.bastien@gmail.com>
2023-09-24 21:32:10 +0200

    CPP

---
+++ 11 - CPP/CPP Module 01/ex04/main.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tastybao <tastybao@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/23 18:35:23 by tastybao          #+#    #+#             */
+/*   Updated: 2023/09/23 18:57:14 by tastybao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include ""
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 4)
+		std::cout << "Please enter a filename and two strings. No more, nos less." << std::endl;
+
+	return (0);
+}
