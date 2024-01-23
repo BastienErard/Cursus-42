5665af51c241d3f44da6fba925dd857a12435e09
TastyBao <erard.bastien@gmail.com>
2024-01-23 21:10:33 +0100

    CPP 06 - Ex. 1

--- 11 - CPP/CPP Module 06/ex00/ScalarConverter.cpp
+++ 11 - CPP/CPP Module 06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: tastybao <tastybao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 18:08:51 by tastybao          #+#    #+#             */
-/*   Updated: 2024/01/20 17:06:24 by tastybao         ###   ########.fr       */
+/*   Updated: 2024/01/23 21:08:24 by tastybao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,11 +36,162 @@ ScalarConverter	&ScalarConverter::operator=(ScalarConverter const & rhs)
 
 void	ScalarConverter::convert(std::string str)
 {
-	std::string	type = getType(str);
-	// to continue
+	std::string	type;
+
+	if (str.empty())
+	{
+		std::cerr << "The string is invalid (empty). Please try again" << std::endl;
+		return;
+	}
+	if (str == "-inff" || str == "+inff" || str == "nanf" || str == "-inf" || str == "+inf" || str == "nan")
+		printSpecialValue(str);
+	else
+	{
+		type = getType(str);
+		chooseConvert(type, str);
+	}
+	return;
 }
 
 std::string	ScalarConverter::getType(std::string str)
 {
+	std::string			type;
+	bool				isFloat = false;
+	bool				isDouble = false;
 
+	if (std::isalpha(str[0]) && str.size() == 1)
+	{
+		type = "char";
+		return (type);
+	}
+	for (int i = 0; i < (int)str.length(); i++)
+	{
+		if ((str[i] == '-' || str[i] == '+') && i == 0)
+			i++;
+		if (std::isdigit(str[i]))
+			continue;
+		else if (str[i] == '.' && !isDouble)
+			isDouble = true;
+		else if (str[i] == 'f' && isDouble && !isFloat)
+		{
+			isDouble = false;
+			isFloat = true;
+		}
+		else
+		{
+		type = "error";
+		return (type);
+		}
+	}
+	if (isDouble)
+		type = "double";
+	else if (isFloat)
+		type = "float";
+	else
+		type = "int";
+	return (type);
+}
+
+void	ScalarConverter::chooseConvert(std::string type, std::string str)
+{
+	if (type == "char")
+		charToOthers(str);
+	else if (type == "int")
+	{
+		int	nb = std::atoi(str.c_str());
+		intToOthers(nb);
+	}
+	else if (type == "float")
+	{
+		float nb = std::atof(str.c_str());
+		floatToOthers(nb);
+	}
+	else if (type == "double")
+	{
+		double nb = std::atof(str.c_str());
+		doubleToOthers(nb);
+	}
+	else
+		std::cerr << "The conversion seems impossible. Please verify and try again" << std::endl;
+	return;
+}
+
+
+void	ScalarConverter::charToOthers(std::string str)
+{
+	char	c = str[0];
+
+	convertChar(c);
+	convertInt(static_cast<int>(c));
+	convertFloat(static_cast<float>(c));
+	convertDouble(static_cast<double>(c));
+	return;
+}
+
+void	ScalarConverter::intToOthers(int nb)
+{
+	convertChar(static_cast<char>(nb));
+	convertInt(nb);
+	convertFloat(static_cast<float>(nb));
+	convertDouble(static_cast<double>(nb));
+	return;
+}
+
+void	ScalarConverter::floatToOthers(float nb)
+{
+	convertChar(static_cast<char>(nb));
+	convertInt(static_cast<int>(nb));
+	convertFloat(nb);
+	convertDouble(static_cast<double>(nb));
+	return;
+}
+
+void	ScalarConverter::doubleToOthers(double nb)
+{
+	convertChar(static_cast<char>(nb));
+	convertInt(static_cast<int>(nb));
+	convertFloat(static_cast<float>(nb));
+	convertDouble(nb);
+	return;
+}
+
+void	ScalarConverter::convertChar(char c)
+{
+	if ((c >= 0 && c <= 32) || c == 127)
+		std::cout << "char: Non displayable" << std::endl;
+	else if (c < 0 || c > 127)
+		std::cout << "char: impossible" << std::endl;
+	else
+		std::cout << "char: '" << c << "'" << std::endl;
+	return;
+}
+
+void	ScalarConverter::convertInt(int nb)
+{
+	if (nb < INT_MIN || nb > INT_MAX)
+		std::cout << "int: impossible:" << std::endl;
+	else
+		std::cout << "int: " << nb << std::endl;
+	return;
+}
+
+void	ScalarConverter::convertFloat(float nb)
+{
+	std::cout << "float: " << std::fixed << std::setprecision(1) << nb << "f" << std::endl;
+	return;
+}
+
+void	ScalarConverter::convertDouble(double nb)
+{
+	std::cout << "double: " << std::fixed << std::setprecision(1) << nb << std::endl;
+	return;
+}
+
+void	ScalarConverter::printSpecialValue(std::string str)
+{
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: " << str << "f" << std::endl;
+	std::cout << "double: " << str << std::endl;
+	return;
 }
--- 11 - CPP/CPP Module 06/ex00/ScalarConverter.hpp
+++ 11 - CPP/CPP Module 06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: tastybao <tastybao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 18:09:29 by tastybao          #+#    #+#             */
-/*   Updated: 2024/01/20 17:04:32 by tastybao         ###   ########.fr       */
+/*   Updated: 2024/01/23 21:07:00 by tastybao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 # define SCALARCONVERTER_HPP
 
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <sstream>
 
 class ScalarConverter
 {
@@ -26,7 +29,19 @@ class ScalarConverter
 		~ScalarConverter(void);
 
 		ScalarConverter &operator=(ScalarConverter const & rhs);
+
 		static std::string	getType(std::string str);
+		static void			chooseConvert(std::string type, std::string str);
+		static void			charToOthers(std::string str);
+		static void			intToOthers(int nb);
+		static void			floatToOthers(float nb);
+		static void			doubleToOthers(double nb);
+		static void			convertChar(char c);
+		static void			convertInt(int nb);
+		static void			convertFloat(float nb);
+		static void			convertDouble(double nb);
+		static void			printSpecialValue(std::string str);
+
 
 
 };
