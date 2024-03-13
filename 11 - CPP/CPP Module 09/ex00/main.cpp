b8ee64ff0ce82649ce3fd6877932f6af9d1f3959
TastyBao <erard.bastien@gmail.com>
2024-03-13 18:55:04 +0100

    Starting CPP 09

---
+++ 11 - CPP/CPP Module 09/ex00/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: berard <berard@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/13 17:18:12 by berard            #+#    #+#             */
+/*   Updated: 2024/03/13 18:13:05 by berard           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 2)
+		std::cout << "Please pass only one argument as a parameter (input.txt)" << std::endl;
+	else
+	{
+
+	}
+	return (0);
+}
